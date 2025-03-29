2cc543e05b350bf0ae2b74422f0aaa085949b9db
Abhishek Korde <89412790+abhikorde31@users.noreply.github.com>
2025-03-29 18:43:18 +0530

    main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -2,22 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.0"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "Terraform_Demo"
-  }
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
